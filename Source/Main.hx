3245d82cfd1a57d36feb58902e410f04ac13ab04
Harry French <harry.french@moonforge.com>
2021-08-27 19:05:18 +0100

    maze drawn

--- Source/Main.hx
+++ Source/Main.hx
@@ -8,23 +8,23 @@ class Main extends Sprite
 	var walls:Array<Array<Bool>>;
 	var flats:Array<Array<Bool>>;
 
-	static var wallPc:Float = 0.3;
-	static var flatPc:Float = 0.25;
+	static var wallPc:Float = 0.5;
+	static var flatPc:Float = 0.3;
 
-	static var xDim:Int = 10;
-	static var yDim:Int = 10;
+	static var xDim:Int = 30;
+	static var yDim:Int = 30;
 
 	public function new()
 	{
 		super();
 
+		walls = new Array<Array<Bool>>();
+		flats = new Array<Array<Bool>>();
 		//generate
 		for(y in 0...yDim){
-			walls = new Array<Array<Bool>>();
-			flats = new Array<Array<Bool>>();
+			walls.push(new Array<Bool>());
+			flats.push(new Array<Bool>());
 			for(x in 0...xDim){
-			walls[y] = new Array<Bool>();
-			flats[y] = new Array<Bool>();
 				if(Random.float(0,1)<wallPc){
 					walls[y].push(true);
 				}else{
@@ -35,8 +35,31 @@ class Main extends Sprite
 				}else{
 					flats[y].push(false);
 				}
-				trace(x,y,walls[x][y],flats[x][y]);
 			}
 		}
+
+		var y:Int = 0;
+		var x:Int = 0;
+		var yd:Int = 20;
+		var xd:Int = 20;
+		var thickness:Int=2;
+
+		do{
+			y++;
+			do{
+				x++;
+				if(walls[y-1][x-1]){
+					graphics.beginFill(0x000000);
+					graphics.drawRect((x-1)*xd,y*yd,xd,thickness);
+					graphics.endFill();
+				}
+				if(flats[y-1][x-1]){
+					graphics.beginFill(0x000000);
+					graphics.drawRect(x*xd,y*yd,thickness,-yd);
+					graphics.endFill();
+				}
+			}while(x<xDim);
+			x = 0;
+		}while(y<yDim);
 	}
 }
